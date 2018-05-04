05c2d5d06db69d8ac347724e4431f86234b4f7a9
Alex Postmaa <apostmaa@umich.edu>
2018-05-04 02:43:49 -0400

    Added character select "functionality"

--- SUPER SMASH BOIS/Assets/SCRIPTS/CharacterSelect.cs
+++ SUPER SMASH BOIS/Assets/SCRIPTS/CharacterSelect.cs
@@ -8,9 +8,14 @@ public class CharacterSelect : MonoBehaviour {
 	void Start () {
 		
 	}
-	
+    public GameObject CharacterIcon;
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetKeyDown(KeyCode.Keypad1) || Input.GetKeyDown(KeyCode.Alpha1))
+        {
+
+            Debug.Log("Ben");
+            Instantiate(CharacterIcon, new Vector3(-5.25f,-3.25f,0), Quaternion.identity);
+        }
+    }
 }
